42eed74db994c74656fd3ea2053fc4ee7fc6dc77
zr8012121 <657977310@qq.com>
2019-06-12 19:55:00 +0800

    加了一个登陆与注册页面  还未正式修改 仅仅一个前台Demo

---
+++ LoveTT/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoveTT.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginHome()
+        {
+            return View();
+        }
+
+
+        public ActionResult Register() {
+
+
+            return View();
+        }
+
+    }
+}
